757c694055a31b0985a21fcba55f339d2c9cfc49
Dominik Stenka <dominik.stenka2309@outlook.com>
2023-03-18 23:49:49 +0100

    Projektliste

--- src/components/projects/ProjectList.jsx
+++ src/components/projects/ProjectList.jsx
@@ -13,15 +13,17 @@ const ProjectList = () => {
     }, []);
 
     return projects.length > 0 ? (
-        <div className="w-full bg-white py-16 px-4">
-            <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-4">
-                {projects.map((project) => (
-                    <ProjectItem project={project} />
-                ))}
+        <>
+            <div className="w-full bg-white py-16 px-4">
+                <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-4">
+                    {projects.map((project) => (
+                        <ProjectItem project={project} />
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     ) : (
-        <div className="w-full bg-white py-16 px-4">
+        <div className="w-full bg-[#000300] py-16 px-4">
             <div className="max-w-[1240px] mx-auto text-center">
                 <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
                     Es sind noch keine Projekte von Dominik angelegt worden.
